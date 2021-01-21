e89a150b2f0c8a17a2a0b802a71a42d60122bfbe
Mir Misbah Ali <mirmisbahali@gmail.com>
2021-01-21 20:22:20 +0530

    completed js

--- p2/app.js
+++ p2/app.js
@@ -1,1 +1,24 @@
 const counter = document.querySelector('#number')
+// const decrease = document.querySelector('#decrease')
+// const reset = document.querySelector('#reset')
+// const increase = document.querySelector("#increase")
+
+var count = 0
+
+function decrease() {
+    if (count > 0)
+        count--
+    counter.textContent = count
+}
+
+function increase() {
+    count++
+    counter.textContent = count
+
+}
+
+function reset() {
+    count = 0
+    counter.textContent = count
+
+}
