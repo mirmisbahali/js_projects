5dc208ec59c7c8be27bde7f6aa0c6d991486dda1
Mir Misbah Ali <mirmisbahali@gmail.com>
2021-02-05 19:50:40 +0530

    added fontawesome fa-trash icon in list item

--- todo_app/app.js
+++ todo_app/app.js
@@ -7,9 +7,11 @@ let tasks = null
 const ListItem = (item) => {
     const li = document.createElement('LI');
     const text = document.createTextNode(item);
-
+    const icon = document.createElement('I');
+    icon.classList.add('fas', 'fa-trash')
     li.appendChild(text);
-    li.classList.add('list-group-item');
+    li.appendChild(icon)
+    li.classList.add('list-group-item', 'd-flex', 'justify-content-between', 'px-5');
     return li;
 }
 
